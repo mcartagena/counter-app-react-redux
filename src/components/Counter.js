352d1dd70aa27df92d763f5930a416d1b342b135
mcartagena <marcelocartagenaorellana@gmail.com>
2020-05-01 22:15:42 -0400

    Adding code for counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,38 @@ import PropTypes from 'prop-types'
 
 class Counter extends Component {
   // code here
+	event() {
+		setTimeout(() => {
+				this.props.onIncrement()
+		}, 1000)
+	}
+	render() {
+		return (
+			<div >
+				<p>Clicked: {this.props.value} times</p>
+				<button
+					onClick={this.props.onIncrement}
+				>
+					+
+				</button>
+				<button
+					onClick={this.props.onDecrement}
+				>
+					-
+				</button>
+				<button
+					onClick={this.props.value%2===0?this.props.onDecrement:this.props.onIncrement}
+				>
+					Increment if odd
+				</button>
+				<button
+					onClick={this.event.bind(this)}
+				>
+					Increment if async
+				</button>        
+			</div>
+		)
+	}  
 }
 
 export default Counter
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,25 @@
 // add reducers accordingly
+const CounterTypes = {
+    INCREMENT: 'INCREMENT',
+    DECREMENT: 'DECREMENT'
+}
+
+const initialState = 0
+
+const format = (state = initialState, action) => {
+    // write Reducers to handle the actions.
+    if (action.type) {
+        switch (action.type) {
+            case CounterTypes.INCREMENT:
+                return state + 1;
+            case CounterTypes.DECREMENT:
+                return state - 1;
+            default:
+                return state;
+        }
+    }
+    return 0;
+
+}
+
+export default format
